adde37abeb7aaefee4d5821884b3ca7b5813b182
Lanyi Knight <sinmaplewing@gmail.com>
2012-12-09 13:10:28 +0800

    Build 2 Pipes and fork 1 Process.

--- Ex1.c
+++ Ex1.c
@@ -9,6 +9,27 @@
 int main(){
   int pid[3] = {0};
   int pfd[2][2] = {0};
+  
+  if( pipe(pfd[0]) == ERROR ){
+    perror("The following error occurred");
+    exit(ERROR);
+  }
+  if( pipe(pfd[1]) == ERROR ){
+    perror("The following error occurred");
+    exit(ERROR); 
+  }
+  
+  if( (pid[0] = fork() ) == ERROR ){
+    perror("The following error occurred");
+    exit(ERROR); 
+  }
+  if( pid[0] == 0 ){
+  
+  }
+  else{
+    if( (pid[1] = fork()) == ERROR ){
+    }
+  }
 
   return 0;
 }
