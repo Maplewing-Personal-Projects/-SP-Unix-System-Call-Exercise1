201b1217ef591c164ad8a8f914b8c3ec162039c2
Lanyi Knight <sinmaplewing@gmail.com>
2012-12-09 13:04:21 +0800

    Basic Setting Up

---
+++ Ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define ERROR (-1)
+#define PIPE_READ 0
+#define PIPE_WRITE 1
+#define STDIN 0
+#define STDOUT 1
+
+int main(){
+  int pid[3] = {0};
+  int pfd[2][2] = {0};
+
+  return 0;
+}
